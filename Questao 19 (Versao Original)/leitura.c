16579403e7fd8676c6313f01ff129b84cd026931
Mateus Aranha <rochamateusa777@gmail.com>
2026-02-04 15:57:13 -0300

    Corrigir a função 'get_csv_field'
    Corrigir a função 'get_csv_field' no arquivo 'leitura.c'.
    
    O principal motivo dessa troca é porque descobri que a função 'strtok' utilizada pela minha função ignora os campos vazios da linha. Como no arquivo 'results.csv' a coluna das medalhas são vazias (,,) se o atleta não for medalhista, 'strtok' >pegava as informações do próximo campo< achando que era da coluna de medalhas.

--- Questao 19 (Versao Original)/leitura.c
+++ Questao 19 (Versao Original)/leitura.c
@@ -9,19 +9,19 @@
 
 // Arquivo de ATLETAS ('bios.csv'):
 // Col 1: Sex | Col 3: Name | Col 4: Born | Col 7: ID
-# define A_COL_GENERO 1
-# define A_COL_NOME 3
-# define A_COL_NASCIMENTO 4
-# define A_COL_ID 7
+#define A_COL_GENERO 1
+#define A_COL_NOME 3
+#define A_COL_NASCIMENTO 4
+#define A_COL_ID 7
 
 // Arquivo de RESULTADOS ('results.csv'):
-// Col 0: Games | Col 4: Medal | Col 6: ID | Col 8 Discipline
-# define R_COL_EDICAO 0
-# define R_COL_MEDALHA 4
-# define R_COL_ID 6
-# define R_COL_ESPORTE 8
+// Col 0: Games | Col 4: Medal | Col 6: ID | Col 8: Discipline
+#define R_COL_EDICAO 0
+#define R_COL_MEDALHA 4
+#define R_COL_ID 6
+#define R_COL_ESPORTE 8
 
-# define BUFFER_SIZE 4096           // Aumentado para garantir a leitura de linhas longas.
+#define BUFFER_SIZE 4096           // Aumentado para garantir a leitura de linhas longas.
 
 
 
@@ -67,17 +67,35 @@ int extrair_ano_string ( char* string ) {
 // Função para obter o campo específico de uma linha CSV:
 char* get_csv_field( char* line, int number ) {
     char* dup = strdup(line);
-    char* tok;
     char* result = NULL;
-    int i = 0;
+    char* ptr = dup;
 
-    for ( tok = strtok(dup, ",;\n") ; tok ; tok = strtok(NULL, ",;\n") ) {
-        if ( i == number ) {
-            result = strdup(tok);
-            break;
+    // Avançar N ('number') vezes pelas virgulas do arquivo .csv.
+    for ( int i = 0 ; i < number ; i++ ) {
+        ptr = strchr(ptr, ',');
+        if ( !ptr ) {
+            // Se a String acabar antes de chegar na coluna desejada:
+            free(dup);
+            return NULL;
         }
-        i++;
+        ptr++;                                                                          // Pular a vírgula p/ ir ao próximo campo.
     }
+
+    // 'ptr' aponta para o ínicio do campo. Para achar onde o campo termina (próx. ',' ou final da linha):
+    char* end = strchr(ptr, ',');
+
+    if ( end ) {
+        *end = '\0';                                                                    // Corta a string na vírgula.
+
+    } else {
+        // Se não houver mais vírgula, remove o '\n' do final.
+        end = strchr(ptr, '\n');
+        if ( end ) *end = '\0';                                                         // Troca a quebra de linha por 'vazio'.
+    }
+
+    // Se o campo estiver vazio (,,), ptr já aponta para \0. strdup cria a string vazia
+    result = strdup(ptr);
+
     free(dup);
     return result;
 }
@@ -89,7 +107,7 @@ char* get_csv_field( char* line, int number ) {
 
 // Função para carregar os dados dos atletas:
 int carregar_atletas ( const char* caminho_arquivo, Atleta *banco_dados ) {
-    FILE* f = fopen(caminho_arquivo, "r");
+    FILE* f = fopen(caminho_arquivo, "r");                                              // Abrir arquivo para leitura.
 
     if ( !f ) {
         printf( "Erro ao abrir arquivo de atletas: &s\n", caminho_arquivo );
@@ -99,9 +117,10 @@ int carregar_atletas ( const char* caminho_arquivo, Atleta *banco_dados ) {
     char linha[BUFFER_SIZE];
     int contador = 0;
 
-    // Pular cabeçalho.
+    // Pular cabeçalho do aquivo.
     fgets(linha, BUFFER_SIZE, f);
 
+    // Loop de leitura ("Parsing").
     while ( fgets(linha, BUFFER_SIZE, f) ) {
         char* s_id = get_csv_field(linha, A_COL_ID);
         char* s_nome = get_csv_field(linha, A_COL_NOME);
@@ -147,4 +166,31 @@ int carregar_atletas ( const char* caminho_arquivo, Atleta *banco_dados ) {
     
     fclose(f);
     return contador;                                                                    // Retornar o total de atletas lidos.
+}
+
+// Função para processar os resultados dos medalhistas:
+int processar_resultados ( const char* caminho_arquivo, Atleta* banco_dados, Medalhista* lista_final ) {
+    FILE* f = fopen(caminho_arquivo, "r");                                              // Abrir aquivo no modo leitura.
+    if ( !f ) {
+        printf( "Erro ao abrir o arquivo de resultados: %s\n", caminho_arquivo );
+        return 0;
+    }
+
+    char linha[BUFFER_SIZE];
+    int contador = 0;
+
+    // Pular cabeçalho do arquivo.
+    fgets(linha, BUFFER_SIZE, f); 
+    
+    // Loop de leitura.
+    while ( fgets(linha, BUFFER_SIZE, f) ) {
+        char* s_id = get_csv_field(linha, R_COL_ID);
+        char* s_edicao = get_csv_field(linha, R_COL_EDICAO);
+        char* s_medalha = get_csv_field(linha, R_COL_MEDALHA);
+        char* s_esporte = get_csv_field(linha, R_COL_ESPORTE);
+
+        // Filtrar se o atleta é medalhista:
+        // if ( s_medalha && strcmp(s_medalha, " "))
+    }
+       
 }
