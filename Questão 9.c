d4f7006bbc991e0016ef94f778c693729c0696a4
EdmSouza <eduardoguimara321@gmail.com>
2026-02-04 00:32:20 -0300

    Defini a estrutura principal

---
+++ Questão 9.c
@@ -0,0 +1,27 @@
+/*Calcule quantos atletas participaram das últimas 10 edições dos Jogos Olímpicos e analise a evolução
+desse número ao longo do tempo, separando Verão e Inverno.*/
+
+
+typedef struct{
+    int ano;
+    int qtdatlet;
+}EdicaoPadrao;
+
+
+int fcalatletasultimos10an(){ //Por boa prática divido as açoes da funçao principal em outra funçao mais especifica
+    EdicaoPadrao verao[10]; //Para isso eu pego a estrutura de uma unica ediçao de olimpiada e divido preencho um array de tamanho 10
+    EdicaoPadrao inverno[10];
+
+
+}
+
+
+int main()
+{
+    FILE* arquivo = fopen("results.csv","r"); //Por segurança verifico se o ponteiro nao está pegando lixo na memória
+    if(arquivo == NULL){ //Com isso evito um retorno nao esperado, no caso, que nao seja abrir o arquivo
+        printf("Não foi possível executar o arquivo");
+        return 1;
+    }
+    
+}
