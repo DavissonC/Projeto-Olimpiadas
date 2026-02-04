37a68828893db7be32314746387369b2f3f7eb81
Mateus Aranha <rochamateusa777@gmail.com>
2026-02-04 00:11:47 -0300

    Adicionar esqueleto inicial para a "Questao 19 (Versão Original)"
    Adicionar esqueleto inicial para a "Questao 19 (Versão Original)": adicionar dados.h, leitura.h/c, analise.h/c, e main.c.

---
+++ Questao 19 (Versao Original)/dados.h
@@ -0,0 +1,14 @@
+/* Nesse arquivo, irei pré-instânciar os DADOS que utilizarei no programa para tornar o código modularizado e organizado 
+   
+   @author Mateus Aranha - (github.com/matt-aranha)
+*/
+
+
+# ifndef DADOS_H              // Se não foi definido...
+# define DADOS_H              // Agora define.
+
+// Struct Atleta:
+
+// Struct Resultado:
+
+# endif                         // Fim do if.
---
+++ Questao 19 (Versao Original)/leitura.h
@@ -0,0 +1,18 @@
+/* Nesse arquivo, irei pré-instânciar as FUNÇÕES QUE TRATARÃO OS DADOS oriundos do .csv
+
+   @author Mateus Aranha - (github.com/matt-aranha)
+*/
+
+
+# ifndef LEITURA_H              // Se não foi definido...
+# define LEITURA_H              // Agora define.
+
+// f_abrir_arquivo
+
+// f_fazer_split_string
+
+// f_parse_atleta
+
+// f_parse_resultado
+
+# endif                         // Fim do if.
