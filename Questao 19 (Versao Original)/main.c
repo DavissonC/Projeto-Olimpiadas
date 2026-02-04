0c43c40e28f5cdc9ca5fad66ec28d01d85870a5a
Mateus Aranha <rochamateusa777@gmail.com>
2026-02-04 18:56:51 -0300

    Adicionando a documentação da Questão 19 (Versão Original)
    Misturei os estilos "Box" e "Doxygen" para fazer uma documentação bonitinha do programa que fiz para solucionar a questão 19.

--- Questao 19 (Versao Original)/main.c
+++ Questao 19 (Versao Original)/main.c
@@ -1,18 +1,43 @@
 /*
-    Para a lógica do problema, precisarei:
-      1. Checar se receberam medalhas;
-      2. Calcular idade que tinha quando ganhou medalha (Ano da sua última olimpíada - Ano de nascimento);
-      3. Ordenar do mais velho ao mais novo;
-      4. Separar por gênero;
-      5. Exibir resultados.
+ * ==========================================================================================================
+ * @brief OLYMPIC DATA ANALYSIS / ANÁLISE DE DADOS OLÍMPICOS
+ * ==========================================================================================================
+ * 
+ * @section [PT] DESCRIÇÃO DA QUESTÃO 19 (VERSÃO ORIGINAL):
+ * Este programa processa bases de dados históricos das Olímpiadas (1896-2022) para responder
+ * a seguinte pergunta não-trivial — proposta pelo Prof. Dr. Kalil Bispo: "Faça o ranking dos
+ * 10 atletas medalhistas mais velhos de todos os tempos que participaram das olimpíadas, por
+ * gênero."
+ * 
+ * ARQUIVOS DE ENTRADA:
+ * - "bios.csv" (Dados Demográficos dos Atletas)
+ * - "results.csv" (Resultados e Medalhas dos Atletas)
+ * Ambos arquivos foram retirados do seguinte repositório: https://github.com/KeithGalli/Olympics-Dataset
+ * 
+ * ----------------------------------------------------------------------------------------------------------
+ * 
+ * @section [EN] DESCRIPTION OF QUESTION 19 (ORIGINAL VERSION):
+ * This program processes historical Olympic datasets (1896-2022) to answer the following non-trivial
+ * question, proposed by Professor Kalil Bispo, Ph.D.: "Rank the 10 oldest Olympic medal-winning athletes
+ * of all time, by gender."
+ * 
+ * INPUT FILES:
+ * - "bios.csv" (Demographic data of the Athletes)
+ * - "results.csv" (Results and medals of the Athletes)
+ * Both files were taking of the following repository: https://github.com/KeithGalli/Olympics-Dataset
+ * 
+ * ==========================================================================================================
+ * @author Mateus Aranha (github.com/matt-aranha)
+ * @version 1.0
+ * ==========================================================================================================
 */
 
 
-# include <stdio.h>
-# include <string.h>
-# include "dados.h"
-# include "leitura.h"
-# include "analise.h"
+#include <stdio.h>
+#include <string.h>
+#include "dados.h"
+#include "leitura.h"
+#include "analise.h"
 
 
 
